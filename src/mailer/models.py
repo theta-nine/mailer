176321d8b654bafc038827f55463835b13a8c5a6
jrickman <dev@jrickman.net>
2022-02-21 21:57:03 -0500

    starting automatic testing

--- src/mailer/models.py
+++ src/mailer/models.py
@@ -1,32 +1,27 @@
 from pydantic import BaseModel
-# from email.mime.multipart import MIMEMultipart 
 from typing import Optional
+
 
 class Message(BaseModel):
     recipient: str
-    sender: str 
+    sender: str
     subject: Optional[str] = None
     message: Optional[str] = None
 
     def __str__(self):
         return f"Email from {self.sender} to {self.recipient} with subject {self.subject}\n{self.message}"
-    
+
     def __repr__(self):
         return str({
-            'recipient':{self.recipient}, 
-            'sender' :{self.sender}, 
-            'subject' :{self.subject}, 
-            'message' :{self.message}
+            'recipient': {self.recipient},
+            'sender': {self.sender},
+            'subject': {self.subject},
+            'message': {self.message}
         })
-        # return f"{'recipient':{self.recipient}, 'sender' :{self.sender}, 'subject' :{self.subject}, 'message' :{self.message}}"
+
 
 class MailServer(BaseModel):
     port_number: int
     server: str
-    username: str 
+    username: str
     password: str
-
-
-
-
-    
